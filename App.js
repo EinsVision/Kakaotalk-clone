e89010ebcc8b6e299700c78050aafa5fa8ec9556
Changdae <einsvision@gmail.com>
2021-02-26 16:22:30 +0900

    building rester function

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import { createStackNavigator } from '@react-navigation/stack';
+import RegisterScreen from './screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
@@ -21,6 +22,10 @@ export default function App() {
         <Stack.Screen
           name='Login' 
           component={LoginScreen}
+        />
+        <Stack.Screen
+          name='Register' 
+          component={RegisterScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
