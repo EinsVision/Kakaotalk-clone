0ca7c787881d6cc1a1d534ebd11d6c701692d283
Changdae <einsvision@gmail.com>
2021-02-26 13:13:23 +0900

    installed npm install react-native-gesture-handler

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Let's build Kakaotalk!🚀✨🎈⭐🛵</Text>
       <StatusBar style="auto" />
+      <Text>Let's build Kakaotalk!🚀✨🎈⭐🛵</Text>
     </View>
   );
 }
