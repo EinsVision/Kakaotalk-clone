6f7e45aff0132fa088595bf27ae4433510c0c6d4
Changdae <einsvision@gmail.com>
2021-02-26 14:10:31 +0900

    complete development setting on React Native

--- App.js
+++ App.js
@@ -3,10 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Screen name='Login' component={LoginScreen} />
       <View style={styles.container}>
         <StatusBar style="auto" />
         <Text>Let's build Kakaotalk!🚀✨🎈⭐🛵</Text>
