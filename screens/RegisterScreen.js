7b5d38f56e427b1645467c1a7ad6aa45428380de
Changdae <einsvision@gmail.com>
2021-02-26 17:01:03 +0900

    building Rester Screen with register function

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'react-native'
+import { Button } from 'react-native'
 import { KeyboardAvoidingView } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import { Input } from 'react-native-elements'
 
 const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  const register = () => {
+
+  }
+
   return (
     <KeyboardAvoidingView behavior='padding' style={styles.container}>
       <StatusBar style='light'/>
@@ -15,8 +25,36 @@ const RegisterScreen = ({ navigation }) => {
       <View style={styles.inputContainer}>
         <Input
           placeholder='Full name'
+          autoFocus
+          type='text'
+          value={name}
+          onChangeText={ (text) => setName(text) }
+        />
+        <Input
+          placeholder='Email'
+          type='email'
+          value={email}
+          onChangeText={ (text) => setEmail(text) }
+        />
+        <Input
+          placeholder='Password'
+          type='password'
+          secureTextEntry
+          value={password}
+          onChangeText={ (text) => setPassword(text) }
+        />
+        <Input
+          placeholder='Profile Picture URL (optional)'
+          type='text'
+          value={imageUrl}
+          onChangeText={ (text) => setImageUrl(text) }
+          onSubmitEditing={register}
         />
       </View>
+      <Button 
+        title='Register'
+        onPress={register}
+      />
     </KeyboardAvoidingView>
   )
 }
