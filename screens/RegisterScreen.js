dc077c7f708fc10544de68e3bd7b70431bcf0715
Changdae <einsvision@gmail.com>
2021-02-26 16:42:03 +0900

    building register function

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
+import { KeyboardAvoidingView } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
+import { Input } from 'react-native-elements'
 
 const RegisterScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>I am a Register Screen.</Text>
-    </View>
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <StatusBar style='light'/>
+      <Text h3 style={{ marginBottom:50 }}>
+        Create a Kakaotalk account
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder='Full name'
+        />
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+
+  },
+  inputContainer: {
+
+  },
+});
